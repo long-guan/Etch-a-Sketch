9bab4c252c9dc2210fbd32bb1b9d87eb1ed597c6
Long G <long.w.guan@gmail.com>
2022-07-06 23:07:14 -0700

    Add functional grid display that updates as it scrolls

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const gridSizeDisplay = document.querySelector("#gridSizeDisplay");
 
 gridSizeDisplay.innerText = `${gridSize.value}x${gridSize.value}`;
 
-
-gridSize.addEventListener("change", function() {
-    console.log(gridSize.value);
-    gridSizeDisplay.innerText = `${gridSize.value}x${gridSize.value}`;
-})
+function showGrid(newVal) {
+    gridSizeDisplay.innerHTML = `${newVal}x${newVal}`;
+}
