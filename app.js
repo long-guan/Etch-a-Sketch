34b137ee770ecfcecd1c2d01ab82d9011509c7ae
Long G <long.w.guan@gmail.com>
2022-07-11 20:00:23 -0700

    fix grid to display correct rows and columns

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const gridSize = document.querySelector("#gridSize");
 const gridSizeDisplay = document.querySelector("#gridSizeDisplay");
 const gridContainer = document.querySelector("#gridContainer");
 let previousGridSize = 0;
-
+const squareStyle = document.querySelector("#square-style");
 
 
 // initializes the grid
@@ -20,13 +20,13 @@ function createGrid(gridSize) {
     previousGridSize = gridSize;
     for (let i = 1; i <= gridSize; i++) {
         for (let j = 1; j <= gridSize; j++) {
-            console.log(`createGrid${i}`);
             let square = document.createElement("div");
             let text = document.createTextNode(`${i}`);
             square.appendChild(text);
             square.classList.add("square-style");
-            // square.style.width = 1000/gridSize.value;
-            // square.style.height = 1000/gridSize.value;
+            let squareStyle = document.querySelector("square-style");
+            square.style.width = 500/gridSize + "px";
+            square.style.height = 500/gridSize + "px";
             gridContainer.appendChild(square);
         }
     }
@@ -37,7 +37,6 @@ function createGrid(gridSize) {
 function deleteGrid(gridContainer) {
     for (let i = 1; i <= previousGridSize; i++) {
         for (let j = 1; j <= previousGridSize; j++) {
-            console.log(`deleteGrid${i}`);
             gridContainer.removeChild(gridContainer.firstElementChild);
         }
     }
