b121600d421b3a1264d21e104f8e1364f0d02f22
Long G <long.w.guan@gmail.com>
2022-07-11 20:22:45 -0700

    add working drawing grid

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const gridSizeDisplay = document.querySelector("#gridSizeDisplay");
 const gridContainer = document.querySelector("#gridContainer");
 let previousGridSize = 0;
 const squareStyle = document.querySelector("#square-style");
+let mouseDown = false;
 
 
 // initializes the grid
@@ -16,23 +17,26 @@ function showGrid(newVal) {
 }
 
 
+function reSizeSquare(square, gridSize) {
+    square.style.width = 500/gridSize + "px";
+    square.style.height = 500/gridSize + "px";
+}
+
+
 function createGrid(gridSize) {
     previousGridSize = gridSize;
     for (let i = 1; i <= gridSize; i++) {
         for (let j = 1; j <= gridSize; j++) {
             let square = document.createElement("div");
-            let text = document.createTextNode(`${i}`);
-            square.appendChild(text);
+            // let text = document.createTextNode(`${i}`);
+            // square.appendChild(text);
             square.classList.add("square-style");
-            let squareStyle = document.querySelector("square-style");
-            square.style.width = 500/gridSize + "px";
-            square.style.height = 500/gridSize + "px";
+            reSizeSquare(square, gridSize);
+            addEventListener(square);
             gridContainer.appendChild(square);
         }
     }
 }
-
-
 
 function deleteGrid(gridContainer) {
     for (let i = 1; i <= previousGridSize; i++) {
@@ -42,8 +46,22 @@ function deleteGrid(gridContainer) {
     }
 }
 
+function addEventListener(square) {
+    square.addEventListener('mousedown', function() {
+        // square.style.backgroundColor = "black";
+        mouseDown = true;
+    })
+    square.addEventListener('mouseup', function() {
+        mouseDown = false;
+    })
+    square.addEventListener('mouseenter', function() {
+        if (mouseDown == true) {
+            square.style.backgroundColor = "black";
+        }
+    })
+}
+
 gridSize.addEventListener("change", function() {
     deleteGrid(gridContainer);
     createGrid(gridSize.value);
-    gridWidthHeight = 1000/gridSize.value;
 })
