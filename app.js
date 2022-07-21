48e56eb301c699ba0e55f80de2a6e1e7ef4d86da
Long G <long.w.guan@gmail.com>
2022-07-20 23:25:04 -0700

    Sketch Pad functionality is fully added. Each mode works when turned off and on

--- app.js
+++ app.js
@@ -14,21 +14,33 @@ let gradientMode = false;
 rainbowMode.addEventListener('click', function() {
     if (plurMode == false) {
         plurMode = true;
-        console.log("plurMode is true");
+        rainbowMode.style.backgroundColor = "gray";
+        gradientMode = false;
+        gradient.style.backgroundColor = "";
+        addEventListForDefaultMode()
+        console.log("rainbowMode is true");
     } else {
         plurMode = false;
-        console.log("plurMode is false");
+        rainbowMode.style.backgroundColor = "";
+        console.log("rainbowMode is false");
     }
 })
 
 // toggles gradient on or off
 gradient.addEventListener('click', function() {
     if (gradientMode == false) {
+    // removes eventlisteners for default mode and add for gradient mode
         gradientMode = true;
-        addEventlistToGradientMode();
+        gradient.style.backgroundColor = "gray";
+        plurMode = false;
+        rainbowMode.style.backgroundColor = "";
+        addEventListForGradientMode();
         console.log("gradientMode is true")
     } else {
+    // removes eventlistener for gradient mode and for default mode
         gradientMode = false;
+        gradient.style.backgroundColor = "";
+        addEventListForDefaultMode()
         console.log("gradientMode is false")
     }
 })
@@ -48,6 +60,24 @@ function showGrid(newVal) {
 function reSizeSquare(square, gridSize) {
     square.style.width = 500/gridSize + "px";
     square.style.height = 500/gridSize + "px";
+}
+
+function addEventListener(square) {
+    if (gradientMode == true) {
+        console.log("gradient")
+        square.addEventListener('mousedown', paint1ClickGrad)
+        square.addEventListener('mouseup', function() {
+            mouseDown = false;
+        })
+        square.addEventListener('mouseenter', paintMouseDownGrad)
+    } else { // rainbow mode and normal mode
+        console.log("non gradient");
+        square.addEventListener('mousedown', paint1ClickDefault, {once: true})
+        square.addEventListener('mouseup', function() {
+            mouseDown = false;
+        })
+        square.addEventListener('mouseenter', paintMouseDownDefault, {once: true})
+    }
 }
 
 // creates row of divs based on gridsize, styling is set to wrap and each div is sized width and height wise to fit the row and columns.
@@ -119,7 +149,7 @@ function paintMouseDownDefault() {
     }
 }
 
-// adds 0.1 to the div's opacity
+// adds 0.1 to the div's opacity, removes event listener when opacity reaches 1
 function increaseOpacity(square) {
     if (square.style.opacity == "") {
         console.log("set opacity to 0.1");
@@ -135,39 +165,53 @@ function increaseOpacity(square) {
     }
 }
 
-function addEventListener(square) {
-    if (gradientMode == true) {
-        console.log("gradient")
-        square.addEventListener('mousedown', paint1ClickGrad)
-        square.addEventListener('mouseup', function() {
-            mouseDown = false;
-        })
-        square.addEventListener('mouseenter', paintMouseDownGrad)
-    } else { // rainbow mode and normal mode
-        console.log("non gradient");
-        square.addEventListener('mousedown', paint1ClickDefault, {once: true})
-        square.addEventListener('mouseup', function() {
-            mouseDown = false;
-        })
-        square.addEventListener('mouseenter', paintMouseDownDefault, {once: true})
-    }
-}
-
-// removes eventlistener for divs that didn't have its eventlistener triggered
-function removeEventListGrad(square) {
+// removes eventlistener for divs that didn't have its eventlistener triggered in default mode
+function removeEventListForDefault(square) {
     if (square.style.opacity == "") {
         square.removeEventListener('mousedown', paint1ClickDefault);
         square.removeEventListener('mouseenter', paintMouseDownDefault);
     }
 }
 
-// when switching between default mode and gradient mode, check each div to see if the default eventlistener was activated, if not remove the one-occurence event and add the gradient mode event to the div
-function addEventlistToGradientMode() {
+// removes all eventlistener for divs in gradient mode
+function removeEventListForGrad(square) {
+    square.removeEventListener('mousedown', paint1ClickGrad);
+    square.removeEventListener('mouseenter', paintMouseDownGrad);
+}
+
+// add eventlistener for gradient mode for divs hasn't been painted or for divs that hasn't reached full opacity
+function checkBeforeAddEventListForGrad(square) {
+    if (square.style.backgroundColor == "") {
+        addEventListener(square);
+    } else if (square.style.opacity > 0 && square.style.opacity < 1) {
+        addEventListener(square);
+    }
+}
+
+// add eventlistener for default mode for divs hasn't been painted
+function checkBeforeAddEventListForDefault(square) {
+    if (square.style.opacity == "") {
+        addEventListener(square);
+    }
+}
+
+// when switching from default mode to gradient mode, check each div to see if the default eventlistener was activated, if not remove the one-occurence event and add the gradient mode event to the div
+function addEventListForGradientMode() {
     let squareList = document.querySelectorAll(".square-style");
     for (let i = 0; i < squareList.length; i++) {
         let square = squareList[i]
-        removeEventListGrad(square);
-        addEventListener(square);
+        removeEventListForDefault(square);
+        checkBeforeAddEventListForGrad(square);
+    }
+}
+
+// when switching from gradient mode to default mode, remove all eventlisteners for gradient mode and add eventlistener for default mode
+function addEventListForDefaultMode() {
+    let squareList = document.querySelectorAll(".square-style");
+    for (let i = 0; i < squareList.length; i++) {
+        let square = squareList[i]
+        removeEventListForGrad(square);
+        checkBeforeAddEventListForDefault(square);
     }
 }
 
